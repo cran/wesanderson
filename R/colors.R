8ee19ddffcb3d67ed3170e92ad7125c2be99bff5
Karthik Ram <karthik.ram@gmail.com>
2023-10-31 16:50:02 +0000

    version 0.3.7

--- R/colors.R
+++ R/colors.R
@@ -11,6 +11,7 @@ wes_palettes <- list(
   Royal1 = c("#899DA4", "#C93312", "#FAEFD1", "#DC863B"),
   Royal2 = c("#9A8822", "#F5CDB4", "#F8AFA8", "#FDDDA0", "#74A089"),
   Zissou1 = c("#3B9AB2", "#78B7C5", "#EBCC2A", "#E1AF00", "#F21A00"),
+  Zissou1Continuous = c("#3A9AB2", "#6FB2C1", "#91BAB6", "#A5C2A3", "#BDC881", "#DCCB4E", "#E3B710", "#E79805", "#EC7A05", "#EF5703", "#F11B00"),
   Darjeeling1 = c("#FF0000", "#00A08A", "#F2AD00", "#F98400", "#5BBCD6"),
   Darjeeling2 = c("#ECCBAE", "#046C9A", "#D69C4E", "#ABDDDE", "#000000"),
   Chevalier1 = c("#446455", "#FDD262", "#D3DDDC", "#C7B19C"),
@@ -22,7 +23,11 @@ wes_palettes <- list(
   GrandBudapest1 = c("#F1BB7B", "#FD6467", "#5B1A18", "#D67236"),
   GrandBudapest2 = c("#E6A0C4", "#C6CDF7", "#D8A499", "#7294D4"),
   IsleofDogs1 = c("#9986A5", "#79402E", "#CCBA72", "#0F0D0E", "#D9D0D3", "#8D8680"),
-  IsleofDogs2 = c("#EAD3BF", "#AA9486", "#B6854D", "#39312F", "#1C1718")
+  IsleofDogs2 = c("#EAD3BF", "#AA9486", "#B6854D", "#39312F", "#1C1718"),
+  FrenchDispatch = c("#90D4CC", "#BD3027", "#B0AFA2", "#7FC0C6", "#9D9C85"),
+  AsteroidCity1 = c("#0A9F9D", "#CEB175", "#E54E21", "#6C8645", "#C18748"),
+  AsteroidCity2 = c("#C52E19", "#AC9765", "#54D8B1", "#b67c3b", "#175149", "#AF4E24"),
+  AsteroidCity3 = c("#FBA72A", "#D3D4D8", "#CB7A5C", "#5785C1")
 )
 
 #' A Wes Anderson palette generator
@@ -39,7 +44,8 @@ wes_palettes <- list(
 #'   \code{Royal1}, \code{Royal2},  \code{Zissou1}, \code{Darjeeling1},
 #'   \code{Darjeeling2},  \code{Chevalier1} , \code{FantasticFox1} ,
 #'   \code{Moonrise1}, \code{Moonrise2}, \code{Moonrise3}, \code{Cavalcanti1},
-#'   \code{GrandBudapest1}, \code{GrandBudapest2}, \code{IsleofDogs1}, \code{IsleofDogs2}
+#'   \code{GrandBudapest1}, \code{GrandBudapest2}, \code{IsleofDogs1}, \code{IsleofDogs2},
+#'   \code{FrenchDispatch}, \code{AsteroidCity2}, \code{AsteroidCity2}, \code{AsteroidCity3}
 #' @param type Either "continuous" or "discrete". Use continuous if you want
 #'   to automatically interpolate between colours.
 #'   @importFrom graphics rgb rect par image text
@@ -61,6 +67,10 @@ wes_palette <- function(name, n, type = c("discrete", "continuous")) {
   type <- match.arg(type)
 
   pal <- wes_palettes[[name]]
+  if (type == "continuous" && name == "Zissou1") {
+    pal <- wes_palettes[["Zissou1Continuous"]]
+  }
+  
   if (is.null(pal))
     stop("Palette not found.")
 
